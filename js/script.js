070b16aa334bb1a50037856aa3a4b449d1e21474
ismoil77 <ismoillmirabbosoff@gmail.com>
2021-07-04 12:46:33 +0500

    some changes

---
+++ js/script.js
@@ -0,0 +1,39 @@
+$(document).ready(function(){
+    $('.product__carousel').owlCarousel();    
+    $('.more__carousel').owlCarousel();
+
+});
+
+$('.product__carousel').owlCarousel({
+    loop:false,
+    margin:10,
+    nav:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:4
+        }
+    }
+})
+
+$('.more__carusel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:4
+        }
+    }
+})
